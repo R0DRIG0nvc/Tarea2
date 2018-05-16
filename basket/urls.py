5869f99429c3c3b5356a36e424bf5aaed076289b
asd <asd@asd.com>
2018-05-16 01:44:27 -0400

    avance listar jugadores

---
+++ basket/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basket import views
+
+
+urlpatterns = [
+    path('', views.index, name="player"),
+    path('list', views.list, name="player_list"),
+    path('add', views.add, name="player_add"),
+    path('view/<int:player_id>', views.detail, name="player_detail"),
+]
---
+++ basket/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from basket.models import Player
+from basket.forms import PlayerForm
+from django.shortcuts import redirect
+
+
+def index(request):
+    data = {}
+
+    # SELECT * FROM player
+    data['object_list'] = Player.objects.all().order_by('-id')
+
+    template_name = 'player/list_player.html'
+    return render(request, template_name, data)
+
+
+def add(request):
+    data = {}
+    if request.method == "POST":
+        data['form'] = PlayerForm(request.POST, request.FILES)
+
+        if data['form'].is_valid():
+            # aca el formulario valido
+            data['form'].save()
+
+            return redirect('player_list')
+
+    else:
+        data['form'] = PlayerForm()
+
+    template_name = 'player/add_player.html'
+    return render(request, template_name, data)
+
+
+def list(request):
+    if request.method == 'POST':
+        print ("entro akjshdadna ", request.id)
+    data = {'players': Player.objects.all()}
+    template_name = "player/listar.html"
+    return render(request, template_name, data)
+
+
+def detail(request, player_id):
+
+    data = {}
+    template_name = 'player/detail_player.html'
+
+    # SELECT * FROM player WHERE id = player_id
+    data['player'] = Player.objects.get(pk=player_id)
+    # import pdb;pdb.set_trace()
+
+    return render(request, template_name, data)
